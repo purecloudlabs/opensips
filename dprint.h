056d36e49dac1fdae599d936699ccaacb658b474
Bence Szigeti <bence.szigeti@gohyda.com>
2022-09-27 18:02:42 +0200

    TELECOM-7008: Fix a dangling else

--- dprint.h
+++ dprint.h
@@ -261,9 +261,9 @@ static inline char* dp_time(void)
 		#define LM_GEN2( _facility, _lev, ...) \
 			do { \
 				if (is_printable(_lev)){ \
-					if (log_stderr) \
-						if (log_stderr) dprint(__VA_ARGS__); \
-					else { \
+					if (log_stderr) { \
+						dprint(__VA_ARGS__); \
+					} else { \
 						switch(_lev){ \
 							case L_CRIT: \
 								syslog(LOG_CRIT|_facility, __VA_ARGS__); \
@@ -452,9 +452,9 @@ static inline char* dp_time(void)
 		#define LM_GEN2( _facility, _lev, fmt, args...) \
 			do { \
 				if (is_printable(_lev)){ \
-					if (log_stderr) \
-						if (log_stderr) dprint( fmt, ## args); \
-					else { \
+					if (log_stderr) { \
+						dprint( fmt, ## args); \
+					} else { \
 						switch(_lev){ \
 							case L_CRIT: \
 								syslog(LOG_CRIT|_facility, fmt, ##args); \
