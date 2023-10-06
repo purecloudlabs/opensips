b42faf5fc65372b6f07a221e1762fa03ee43d43f
Darragh Hennessy <darragh.hennessy@genesys.com>
2023-10-06 13:36:00 +0100

    Trying out fix

--- modules/dialog/dlg_hash.c
+++ modules/dialog/dlg_hash.c
@@ -711,18 +711,16 @@ int dlg_update_routing(struct dlg_cell *dlg, unsigned int leg,
 		rr->len,rr->s,
 		contact->len,contact->s );
 
-    if (contact->len) {
-        if (dlg->legs[leg].contact.s)
-            shm_free(dlg->legs[leg].contact.s);
+	if (dlg->legs[leg].contact.s)
+		shm_free(dlg->legs[leg].contact.s);
 
-        dlg->legs[leg].contact.s = shm_malloc(contact->len);
-        if (dlg->legs[leg].contact.s==NULL) {
-            LM_ERR("no more shm mem\n");
-            return -1;
-        }
-        dlg->legs[leg].contact.len = contact->len;
-        memcpy( dlg->legs[leg].contact.s, contact->s, contact->len);
-    }
+	dlg->legs[leg].contact.s = shm_malloc(contact->len);
+	if (dlg->legs[leg].contact.s==NULL) {
+		LM_ERR("no more shm mem\n");
+		return -1;
+	}
+	dlg->legs[leg].contact.len = contact->len;
+	memcpy( dlg->legs[leg].contact.s, contact->s, contact->len);
 	/* rr */
 	if (rr->len) {
 		if (dlg->legs[leg].route_set.s)
