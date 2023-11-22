8e165704abfade5f07d1ba7c0c9406a17ca924f4
Evan <evan.maloney@genesys.com>
2023-11-22 11:41:31 +0000

    Adding some more debugging

--- parser/msg_parser.c
+++ parser/msg_parser.c
@@ -60,7 +60,7 @@
 #include <mem/dmalloc.h>
 #endif
 
-
+#define REDACT_LOG
 #define parse_hname(_b,_e,_h) parse_hname2((_b),(_e),(_h))
 
 /* number of via's encountered */
@@ -710,6 +710,7 @@ int parse_msg(char* buf, unsigned int len, struct sip_msg* msg)
 
 	offset+=rest-tmp;
 	tmp=rest;
+	LM_DBG("LOGGING BEFORE SWITCH STATEMENT:\n");
 	switch(fl->type){
 		case SIP_INVALID:
 			LM_DBG("invalid message\n");
@@ -721,6 +722,7 @@ int parse_msg(char* buf, unsigned int len, struct sip_msg* msg)
 			goto error;
 			break;
 		case SIP_REQUEST:
+			LM_DBG("SWITCH STATEMENT INSIDE SIP_REQUEST:\n");
 			LM_DBG("SIP Request:\n");
 			LM_DBG(" method:  <%.*s>\n",fl->u.request.method.len,
 				ZSRW(fl->u.request.method.s));
@@ -799,10 +801,14 @@ int parse_msg(char* buf, unsigned int len, struct sip_msg* msg)
 	return 0;
 
 error:
-	/* more debugging, msg->orig is/should be null terminated*/
-	LM_ERR("message=<%.*s>\n", (int)len, ZSRW(buf));
-	return -1;
+    /* more debugging, msg->orig is/should be null terminated */
+    LM_DBG("Raw buffer content: <%s>\n", buf);
+    LM_DBG("Testing ZSRW macro: <%s>\n", ZSRW("Test String"));
+    LM_DBG("Length of buffer: %d\n", len);
+    LM_ERR("message=<%.*s>\n", (int)len, ZSRW(buf));
+    return -1;
 }
+
 
 
 
