f0e5e3cc40b74e15d2ca5181fef43549d3ee443d
David Trihy <david.trihy@genesys.com>
2025-12-10 12:59:39 +0000

    Revert "GCVCALLP-2234: Checking maddr before overwriting ruri during loose route"
    This reverts commit a6d07e03bfe19330969bb14b8f61e73d3c2dae5f.

--- modules/rr/loose.h
+++ modules/rr/loose.h
@@ -38,6 +38,9 @@
 #define RR_FLOW_DOWNSTREAM  (1<<0)
 #define RR_FLOW_UPSTREAM    (1<<1)
 
+#define LR_NO_FLAGS 0
+#define LR_ON_SELF  1	
+
 extern int ctx_rrparam_idx;
 extern int ctx_routing_idx;
 
@@ -45,7 +48,7 @@ extern int ctx_routing_idx;
 /*! \brief
  * Do loose routing as per RFC3261
  */
-int loose_route(struct sip_msg* _m);
+int loose_route(struct sip_msg* _m, void* func_flags);
 
 
 /*! \brief
@@ -239,5 +242,29 @@ static inline int is_2rr(str* _params)
 	if ((state == 2) || (state == 3)) return 1;
 	else return 0;
 }
+
+static inline int fixup_lr_flags(void** param)
+{
+	int index, ret = LR_NO_FLAGS;
+	str *flags = (str *)*param;
+
+	for (index=0; index < flags->len; index++) {
+		switch (flags->s[index]) {
+			case ' ':
+				break;
+			case 'l':
+			case 'L':
+				ret |= LR_ON_SELF;
+				break;
+			default:
+				LM_ERR("Invalid flag\n");
+				return -1;
+		}
+	}
+
+	*param = (void *)(long)ret;
+	return 0;
+}
+
 
 #endif /* LOOSE_H */
