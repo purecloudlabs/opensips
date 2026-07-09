ea43ae78c29f0531db6b66cdbe9711f08f180c8d
Bence Szigeti <bence.szigeti@genesys.com>
2026-07-09 11:02:39 +0200

    GCVCALLP-3095: Make HTTP MI interface async

--- modules/httpd/httpd.c
+++ modules/httpd/httpd.c
@@ -73,8 +73,10 @@ struct httpd_cb *httpd_cb_list = NULL;
 char *httpd_receive_buff = NULL;
 int httpd_receive_buff_pos=0;
 
-static const proc_export_t mi_procs[] = {
-	{"HTTPD",  0,  0, httpd_proc, 1,
+int httpd_workers = 1;
+
+static proc_export_t mi_procs[] = {
+	{"HTTPD",  httpd_pre_fork,  httpd_post_fork, httpd_proc, 1,
 		PROC_FLAG_INITCHILD|PROC_FLAG_HAS_IPC|PROC_FLAG_NEEDS_SCRIPT },
 	{NULL, 0, 0, NULL, 0, 0}
 };
@@ -94,6 +96,7 @@ static const param_export_t params[] = {
 	{"auth_realm", STR_PARAM, &auth_realm.s},
 	{"auth_username", STR_PARAM, &auth_username.s},
 	{"auth_password", STR_PARAM, &auth_password.s},
+	{"workers",       INT_PARAM, &httpd_workers},
 	{NULL, 0, NULL}
 };
 
@@ -208,6 +211,12 @@ static int mod_init(void)
 			MIN_POST_BUF_SIZE);
 		return -1;
 	}
+
+	if (httpd_workers < 1) {
+		LM_ERR("invalid 'workers' value %d\n", httpd_workers);
+		return -1;
+	}
+	mi_procs[0].no = httpd_workers;
 	if (buffer.len == 0)
 		buffer.len = (pkg_mem_size/4);
 	LM_DBG("buf_size=[%d]\n", buffer.len);
